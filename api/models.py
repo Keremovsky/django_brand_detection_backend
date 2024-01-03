247279aa71e11b3eb3a468e4818c03ba8573c579
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2024-01-03 19:33:19 +0300

    comments for models and resultIds attribute added

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import json
 
 
+# model for user data
 class User(AbstractUser):
     name = models.CharField(max_length=50, null=True)
     email = models.EmailField(unique=True)
@@ -11,13 +13,22 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
 
+# model that hold old searches made by user
 class HistoryModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="history/")
-    # resultIds
+    resultIds = models.CharField(max_length=200)
     isSaved = models.BooleanField()
 
+    # functions for store and get result ids
+    def setResultsIds(self, ids):
+        self.resultIds = json.dumps(ids)
 
+    def getResultIds(self):
+        return json.loads(self.resultIds)
+
+
+# model that hold data for requests made by user
 class RequestModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="request/")
@@ -29,6 +40,7 @@ class RequestModel(models.Model):
     twitter = models.CharField(max_length=100)
 
 
+# feedbacks from users about searches
 class FeedbackModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     history = models.ForeignKey(HistoryModel, on_delete=models.CASCADE)
