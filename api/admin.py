1554517b1513bd7091b3b30dc5c25cbc052e3777
Omer Furkan Yalcin <frknylcn26@gmail.com>
2024-01-07 22:52:55 +0300

    added history table view for admin panel

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from .models import User, HistoryModel, FeedbackModel, RequestModel
 from django.utils.html import format_html
 
 admin.site.register(User)
-admin.site.register(HistoryModel)
+# admin.site.register(HistoryModel)
 # admin.site.register(FeedbackModel)
 # admin.site.register(RequestModel)
 
@@ -21,5 +21,16 @@ class FeedbackModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'history', 'date', 'description')
     search_fields = ['user__username', 'history__some_field', 'description']
 
+class HistoryModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'display_image', 'resultIds', 'similarities', 'isSaved')
+    list_filter = ('user', 'date', 'isSaved')
+    search_fields = ('user__username', 'resultIds', 'similarities')
+
+    def display_image(self, obj):
+        return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
+
+    display_image.short_description = 'Image'
+
+admin.site.register(HistoryModel, HistoryModelAdmin)
 admin.site.register(RequestModel, RequestModelAdmin)
 admin.site.register(FeedbackModel, FeedbackModelAdmin)
