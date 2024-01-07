b6744bb72c30f3e649d240612dc0a447fe7fd876
Omer Furkan Yalcin <frknylcn26@gmail.com>
2024-01-07 22:41:38 +0300

    admin panel custom view added

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,16 @@ from .models import User, HistoryModel, FeedbackModel, RequestModel
 
 admin.site.register(User)
 admin.site.register(HistoryModel)
-admin.site.register(FeedbackModel)
-admin.site.register(RequestModel)
+# admin.site.register(FeedbackModel)
+# admin.site.register(RequestModel)
+
+class RequestModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date', 'companyName', 'country', 'website', 'twitter')
+    search_fields = ['user__username', 'companyName', 'country']
+
+class FeedbackModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'history', 'date', 'description')
+    search_fields = ['user__username', 'history__some_field', 'description']
+
+admin.site.register(RequestModel, RequestModelAdmin)
+admin.site.register(FeedbackModel, FeedbackModelAdmin)
