4b6d3545ea4baa32e17a22c045a1910ff34dbcf7
Omer Furkan Yalcin <frknylcn26@gmail.com>
2024-01-07 22:46:35 +0300

    fix

--- api/admin.py
+++ api/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import User, HistoryModel, FeedbackModel, RequestModel
+from django.utils.html import format_html
 
 admin.site.register(User)
 admin.site.register(HistoryModel)
 # admin.site.register(FeedbackModel)
 # admin.site.register(RequestModel)
 
+
 class RequestModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'date', 'companyName', 'country', 'website', 'twitter')
+    list_display = ('id', 'user', 'date', 'companyName', 'country', 'website', 'twitter', 'display_image')
     search_fields = ['user__username', 'companyName', 'country']
+
+    def display_image(self, obj):
+        return format_html('<img src="{}" style="width:50px;height:50px;" />', obj.image.url)
+
+    display_image.short_description = 'Image'
 
 class FeedbackModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'history', 'date', 'description')
